35ca8eca49bd623fd85f5b8db78d3966d3e309ed
shazmaan <shazmaan@buffalo.edu>
2019-07-09 07:00:22 +0400

    Approach for Problem2 updated.

--- Problem2.cpp
+++ Problem2.cpp
@@ -9,6 +9,21 @@
 #include <map>
 
 using namespace std;
+/* Approach:
+ * let s = "egg" ; t = "add"
+ * map characters from s to values in t.
+ * to avoid multiple values, we have a hashset as well. Every time we run loop,
+ * we will check if hashset.insert(t[index]) returns true or false. returns true if value inserted
+ * and no duplicates in hashset else false.
+ * if true and s[index] is not in hashmap, then add to hashmap. Otherwise, check is value in hashmap
+ * is same as t[index].
+ *
+ * ex: hashmap = {} ; hashset = {}; index = 0;
+ * iteration 1 : hashmap{'e':'a'} ; hashset{'a'}; index = 1;
+ * iteration 2 : hashmap{'g':'d'} ; hashset{'a','d'}; index =2;
+ * iteration 3 : hashset will return false, so we check hashmap[s[2]] ==t[2] (d==d) then ok else return true.
+ *
+ * */
 
 class Solution{
 private:
