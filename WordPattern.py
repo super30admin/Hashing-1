c97fe2bb2a7eb744c7658141c5bab3c348de62b0
Vishwanath Patil <vishwanath.patil@sjsu.edu>
2019-08-28 11:54:23 -0700

    Completed

--- WordPattern.py
+++ WordPattern.py
@@ -1,3 +1,12 @@
+"""
+The approach here is to maintain two dictionary and map each pattern to each corresponding string.
+while maping and adding values to the dict check if the key is already present in the dict, if so
+check if the key's dict value matches the string's value if not return false same way do the reverse 
+mapping from str to pattern here str as key and pattern as val. check for the same conditions mentioned
+above. finally if the for loop exits without a false this means that the mappings hold true and are valid.
+
+"""
+
 pattern = "abba"
 str = "dog cat cat fish"
 
