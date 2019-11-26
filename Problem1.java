e6937eb01d98aff5de4ab3839eb13af546ac698a
Prateek Dasgupta <dasguptp@oregonstate.edu>
2019-11-26 07:10:15 -0800

    Replacing wrong code in Problem 1

--- Problem1.java
+++ Problem1.java
@@ -1,53 +1,36 @@
 
-/* The idea is to use index to decide which part of the array there will be missing element and then traverse accordingly*/
+/*
+Leetcode Submitted : Yes
+Space Complexity : O(N)
+Time Complexity : O(N*MlogM) #M= String Size
+The idea is to sort string and store in hashmap as key and values as list of string
+*/
 
-class solution{
-    
-    public static void main(String [] args) {
-	//code
-	 int[] nums = {0,1,2,3,5,6};
-	 //int[] nums = {1};
-	System.out.println(search_missing(nums));
-	
-}
-
-public static int search_missing(int[] nums){
-   
-	//Arrays.sort(nums);
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       
+        if(strs == null || strs.length == 0)
+            return new ArrayList<>();
         
-    int l = 0;
-    int r = nums.length - 1;
-        
-        
-    if(nums == null || nums.length < 1)
-        return -1;
-        
-    if(nums.length == 1 && nums[0] == 0)
-        return 1;
-        
-    return search(nums,l,r);
-}
-public static int search(int[] a , int l , int r){
-    int mid = (l + r)/2;
-        
-        if(l <= r){
+        HashMap<String,List<String>> hm = new HashMap<>();
+        for(String s : strs){
+            char[] temp =  s.toCharArray();
+            Arrays.sort(temp);
+            String new_temp = String.valueOf(temp);
             
-            if(a[mid] != mid)
-                return mid;
+            if(hm.containsKey(new_temp)){
+                hm.get(new_temp).add(s);
+                
+            }else{
+                List<String> a = new ArrayList<>();
+                a.add(s);
+                hm.put(new_temp,a);
+            }
             
-            if(a[mid] == mid)
-                return search(a,mid+1, r);
-            else
-                return search(a,l,mid - 1);
-        
         }
-        return -1;
-
-    
+        
+        return new ArrayList(hm.values());
+        
+    }
 }
-    
-    
-    
-}
-
 
