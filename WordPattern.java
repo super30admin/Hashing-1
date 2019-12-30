472b4f40f0444c8a900f20e08004778051a58c98
Siddharth Mehta <mehtasiddharth1991@gmail.com>
2019-12-30 17:47:46 +0530

    Comments Added

--- WordPattern.java
+++ WordPattern.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(n^2) 
+// Space Complexity : Not sure. Need to learn how to calculate.
+// Did this code successfully run on Leetcode : Yes.
+// Any problem you faced while coding this : No.
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
