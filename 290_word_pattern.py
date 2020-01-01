8638b44aa959c003a7d7af922081544a4ae42daf
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-01 08:59:45 -0500

    all problems complete updated as per class

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -1,43 +1,59 @@
 class Solution:
     def wordPattern(self, pattern: str, str: str) -> bool:
         """
-            // Time Complexity: O(m+n)
-                # 'm' is the number of elements in 's' and
-                # 'n' is the number of elements in 't'
+            // Time Complexity: O(n)
+                # 'n' the length of the pattern
             // Space Complexity:
-                O(n) We have a dictionary and the zip list
+                    O(1) Since it is given we will only have
+                    lower case  letters, in the worst case the
+                    size of each dictionary will never exceed 26
             // Did this code successfully run on Leetcode :
-                Yes
+                    Yes
             // Any problem you faced while coding this :
-                Not sure if the time complexities are right.
-                Its tricky!
+                    Time complexities
         """
-        # O(n)
         arr_str = str.split()
-        # O(1)
         if len(pattern) != len(arr_str):
             return False
-        # O(n) and O(1)
-        if len(set(pattern)) != len(set(arr_str)):
-            return False
-        # O(m+n)
-        values = list(zip(pattern, arr_str))
-        pattern_dict = {}
-        # O(m+n)
-        for key, value in values:
-            # O(1) and O(1)
-            if key in pattern_dict and pattern_dict[key] != value:
+        ptn_dict = {}
+        str_dict = {}
+        for i in range(len(pattern)):
+            if pattern[i] not in ptn_dict:
+                ptn_dict[pattern[i]] = arr_str[i]
+            elif ptn_dict[pattern[i]] != arr_str[i]:
                 return False
-            else:
-                # O(1)
-                pattern_dict[key] = value
+
+            if arr_str[i] not in str_dict:
+                str_dict[arr_str[i]] = pattern[i]
+            elif str_dict[arr_str[i]] != pattern[i]:
+                return False
         return True
+        # # O(n)
+        # arr_str = str.split()
+        # # O(1)
+        # if len(pattern) != len(arr_str):
+        #     return False
+        # # O(n) and O(1)
+        # if len(set(pattern)) != len(set(arr_str)):
+        #     return False
+        # # O(m+n)
+        # values = list(zip(pattern, arr_str))
+        # pattern_dict = {}
+        # # O(m+n)
+        # for key, value in values:
+        #     # O(1) and O(1)
+        #     if key in pattern_dict and pattern_dict[key] != value:
+        #         return False
+        #     else:
+        #         # O(1)
+        #         pattern_dict[key] = value
+        # return True
 
 
 if __name__ == '__main__':
     h = Solution()
-    print(h.wordPattern("abba", "dog cat cat dog"))
-    print(h.wordPattern("aaaa", "dog cat cat dog"))
-    print(h.wordPattern("abba", "dog dog dog dog"))
-    print(h.wordPattern("aaaa", "dog cat cat dog"))
-    print(h.wordPattern("aba", "cat cat cat dog"))
+    print(h.wordPattern("abba", "dog cat cat fish"))
+    # print(h.wordPattern("aaaa", "dog cat cat dog"))
+    # print(h.wordPattern("abba", "dog dog dog dog"))
+    # print(h.wordPattern("aaaa", "dog cat cat dog"))
+    # print(h.wordPattern("aba", "cat cat cat dog"))
