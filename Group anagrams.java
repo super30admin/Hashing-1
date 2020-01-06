9cf9c4b7d51b4964b5686aaeebd2ea2e32ea3bb1
BhaveshTadikonda <30873158+BhaveshTadikonda@users.noreply.github.com>
2020-01-05 19:26:10 -0500

    Please review

---
+++ Group anagrams.java
@@ -0,0 +1,22 @@
+// Time complexity 0(nklogk)
+// space complexity 0(n)
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Base case
+        if(strs == null || strs.length==0) return new ArrayList<>();
+        HashMap <String,List<String>> map = new HashMap<>();
+        for (String s : strs){
+            char [] c = s.toCharArray(); 
+            Arrays.sort(c);  
+            String sorted = String.valueOf(c);
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());  
+            }
+            map.get(sorted).add(s);
+        }
+        return new ArrayList<>(map.values());
+        
+        
+    }
+}
