03c32980940c1075bdf6f83a2834a05f7d41456f
KalyaniJawanjal-design <59378872+KalyaniJawanjal-design@users.noreply.github.com>
2020-04-27 16:58:57 -0700

    Add files via upload

---
+++ GroupAnagrams.java
@@ -0,0 +1,26 @@
+// Time Complexity : O(N K Log K)
+// Space Complexity : O(NK)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : COuld not optimize it 
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length==0 || strs==null) return new ArrayList();
+        
+        HashMap<String, List> map = new HashMap<>();
+        
+        for(String s : strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key=String.valueOf(ch);
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList(map.values());
+    }
+}
