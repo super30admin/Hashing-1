3e6fbc377fc79ff5c8609de42679bd5640da4305
Siyami Avci <siyamiavci@gmail.com>
2020-04-27 18:28:06 -0700

    add solution1

---
+++ problem1.js
@@ -0,0 +1,34 @@
+// ## Problem 1:
+
+// Time Complexity : O(n log(n))
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
+// Given an array of strings, group anagrams together.
+
+// Example:
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Output:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+
+// Note:
+// All inputs will be in lowercase.
+// The order of your output does not matter.
+
+var groupAnagrams = function(strs) { 
+    const map = new Map();
+    let res = [];
+    for (let str of strs) {
+        const key = str.split('').sort().join('');
+        if (!map.has(key)) map.set(key, [])
+        map.get(key).push(str);
+    }
+    for(let [k,v] of map) {
+        res.push(v);
+    }
+    return res;
+};
