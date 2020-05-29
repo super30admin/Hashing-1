9618368892b06d26995b89f918dfcf068211b6f7
Tejaswini <tramatejaswini@gmail.com>
2020-05-28 19:37:35 -0700

    Update Desc_ GroupAnagrams-IsomorphicStrings-WordPattern

--- GroupAnagrams_Isomorphic_WordPattern.py
+++ GroupAnagrams_Isomorphic_WordPattern.py
@@ -6,7 +6,9 @@
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # 
-# Used dictionary to store the values of the tuple.
+# As anagrams have same characters with same count I have used dictionary to store the string sorted values of the tuple. 
+#Iterating through the strs list and sorting each string in the list which will be used as a key of dictionary. 
+# 'eat' can be converted to ('a','e','t') and also 'ate' can be converted ('a','e','t') as well which will be added to same dictionary key.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -18,7 +20,9 @@ class Solution:
             d[tuple(sorted(i))].append(i)
         return d.values()
         
-#Second Approach:
+#Second Approach: Here instead of sorting the string which takes nlogn runtime, I have used a list named count, 
+#which I can store the index of the character present in that particular string. I am converting that list to tuple and adding into dictionary as a key. 
+#If the same key present in the dictionary for other string, It will be added to that key as a value.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -41,7 +45,10 @@ class Solution:
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # 
-# Used dictionary to store the mapped pairs and a set to store the already mapped characters.
+# Used dictionary to store the mapped pairs and a set to store the already mapped characters.Initialized the dict and a set. Looping through the elements of both strings and mapping the s key to t value.
+# I am checking if the key is already in the dict, then if the value is equal to char in the string t or not.
+#Second, Checking if the character t is already mapped or not using set. If it is already mapped returning False.
+# Third if the char of s is not present in the dict, add it to the dict. 
         
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
@@ -65,7 +72,11 @@ class Solution:
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # 
-# Used dictionary to store the mapped pairs.    
+# Same as the previous problem. Converted the string to list by splitting at the space character. Checked the base conditions
+# number of characters in pattern and str are equal. ALso the distinct chars and strs are equal, If not return False
+#Used dictionary to store the mapped pairs. Initialized the dict and a set. Looping through the elements of both pattern and strings and mapping the pattern key to strs value.
+# I am checking if the key is already in the dict, then if the value is equal to char in the string t or not.
+# Second if the char of pattern is not present in the dict, add it to the dict.    
     
 class Solution:
     def wordPattern(self, pattern: str, str: str) -> bool:
