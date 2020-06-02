25938b3f5f474803ad30f9eb9a659087f2921ac8
KalyaniJawanjal-design <59378872+KalyaniJawanjal-design@users.noreply.github.com>
2020-06-01 21:56:23 -0700

    Hashing-1 Complete - Please review

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,41 +1,41 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode : No
-// Any problem you faced while coding this : Duplicate values being mapped to unique keys in hashmap
-
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 // Your code here along with comments explaining your approach
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
         if(s.length()!=t.length())
             return false;
-        
-        HashMap<Character, Character> map = new HashMap<>();
+        HashMap<Character, Character> sMap = new HashMap();
+        HashMap<Character, Character> tMap = new HashMap();
         
         for(int i=0; i<s.length(); i++){
-            //if(map.containsKey(s.charAt(i)))
-           // System.out.println("s="+s.charAt(i)+" t="+t.charAt(i));
-            
-            if(map.containsValue(t.charAt(i)) && !map.containsKey(s.charAt(i))){
-                System.out.println("index="+i);
-                map.put(s.charAt(i),'@');
-                System.out.println(map.get(s.charAt(i)));
+            if(!sMap.containsKey(s.charAt(i))){
+                sMap.put(s.charAt(i), t.charAt(i));
             }
-            else
-                map.put(s.charAt(i), t.charAt(i));
+            else{
+                if(sMap.get(s.charAt(i)) == t.charAt(i))
+                    continue;
+                else
+                    return false;
+            }
         }
-        System.out.println(map);
+        
         for(int i=0; i<s.length(); i++){
-            if(map.get(s.charAt(i))!=t.charAt(i)){
-              //  System.out.println(s.charAt(i)+" "+t.charAt(i));
-                return false;
+            if(!tMap.containsKey(t.charAt(i))){
+                tMap.put(t.charAt(i), s.charAt(i));
+            }
+            else{
+                if(tMap.get(t.charAt(i)) == s.charAt(i))
+                    continue;
+                else
+                    return false;
             }
         }
         
         return true;
     }
 }
-
-
