4b7b648a52b1c67cf3749fcbbe7b90777e3af3b2
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-06-02 00:18:17 -0400

    Completed Sprint Hashing-1

---
+++ LeetCode290.java
@@ -0,0 +1,43 @@
+// Time Complexity : O(n) --> where n is length of string
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    HashMap<Character, String> pMap;
+    HashMap<String, Character> sMap;
+    
+    Solution() {
+        pMap = new HashMap<>();
+        sMap = new HashMap<>();
+    }
+    
+    public boolean wordPattern(String pattern, String str) {
+        String strArray[] = str.split(" ");
+        
+        if (pattern.length() != strArray.length) return false;
+        
+        for (int i = 0; i < strArray.length; i++) {
+            char pChar = pattern.charAt(i);
+            String sChar = strArray[i];
+            
+            if (pMap.containsKey(pChar)) {
+                if (!pMap.get(pChar).equals(sChar)) return false;
+            }
+            else {
+                pMap.put(pChar, sChar);
+            }
+            
+            if (sMap.containsKey(sChar)) {
+                if (!sMap.get(sChar).equals(pChar)) return false;
+            }
+            else {
+                sMap.put(sChar, pChar);
+            }
+        }       
+        return true;
+    }
+}
---
+++ LeetCode49.java
@@ -0,0 +1,33 @@
+// Time Complexity : O(nk) --> where n is length of string array and k is length of a particular string in string array
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    int primes[];
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        primes = new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,67,71,73,79,83,89,97,101,103};
+        HashMap<Long, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            long hash = primeProduct(s);
+            if(!map.containsKey(hash)) {
+                map.put(hash, new ArrayList<>());
+            }
+            map.get(hash).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    
+    private long primeProduct(String s) {
+        long result = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            result *= primes[c - 'a']; 
+        }
+        return result;
+    }
+}
