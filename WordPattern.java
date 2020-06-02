2f657feceebe61336351258049b96f40f64bc587
KalyaniJawanjal-design <59378872+KalyaniJawanjal-design@users.noreply.github.com>
2020-06-01 21:52:20 -0700

    Add files via upload

--- WordPattern.java
+++ WordPattern.java
@@ -1,36 +1,47 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode : No
-// Any problem you faced while coding this : Duplicate values are being mapped against unique keys
-
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
 
 // Your code here along with comments explaining your approach
 
 class Solution {
     public boolean wordPattern(String pattern, String str) {
         
-        if(pattern.length()==0 && str.length()==0) return true;
-        if(pattern.length()==0 || str.length()==0) return false;
+        String[] arr = str.split(" ");
+        if(pattern.length()!=arr.length) return false;
         
-      //  if(pattern.length() != str.length()) return false; 
-        
-        String[] strs=str.split("");
-        
-        HashMap<Character, String> map = new HashMap<>();
+        HashMap<Character, String> pmap = new HashMap<>();
+        HashMap<String, Character> smap = new HashMap<>();
         
         for(int i=0; i<pattern.length(); i++){
-            if(!map.containsKey(pattern.charAt(i)) && !map.containsValue(strs[i]))
-                map.put(pattern.charAt(i), strs[i]);
+            if(!pmap.containsKey(pattern.charAt(i))){
+                pmap.put(pattern.charAt(i),arr[i]);
+            }
+            else{
+                if((pmap.get(pattern.charAt(i))).equals(arr[i]))
+                    continue;
+                else{
+                   // System.out.println("1");
+                    return false;
+                }
+            }
         }
-       
-        for(int i=0;i<pattern.length();i++){
-            if(!map.get(pattern.charAt(i)).equals(strs[i])){
-                System.out.println("x");
-                return false;   
+        
+        for(int i=0; i<pattern.length(); i++){
+            if(!smap.containsKey(arr[i])){
+                smap.put(arr[i], pattern.charAt(i));
+            }
+            else{
+                if((smap.get(arr[i])).equals(pattern.charAt(i)))
+                    continue;
+                else{
+                   // System.out.println("2");
+                    return false;
+                }
             }
         }
         
         return true;
-        
     }
 }
