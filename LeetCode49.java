8dc5a52cc897e37be56f9748f0ccf0c2d5606fbd
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-06-02 20:26:19 -0400

    added edge case

--- LeetCode49.java
+++ LeetCode49.java
@@ -10,6 +10,8 @@ class Solution {
     int primes[];
     
     public List<List<String>> groupAnagrams(String[] strs) {
+		if (strs == null || strs.length == 0) return new ArrayList<>();
+			
         primes = new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,67,71,73,79,83,89,97,101,103};
         HashMap<Long, List<String>> map = new HashMap<>();
         for (String s: strs) {
