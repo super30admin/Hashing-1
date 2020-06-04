243fee6d495945a01db1be19c0f5ef473df066e4
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-06-03 18:39:02 -0700

    Updated TC and SC

--- Problem-1.py
+++ Problem-1.py
@@ -1,3 +1,9 @@
+# Time Complexity : O(n*k) , where k is the length of each string inside the array
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+# Your code here along with comments explaining your approach
+
 from collections import defaultdict 
 class Solution:
     # Time = O(n2log(n)) | Space = O(1) 
