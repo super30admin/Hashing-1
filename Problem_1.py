44d2ec314e67551e60965a609b320e049d33b55f
Abhishek Prusty <Abhishek.lan@gmail.com>
2020-06-18 13:01:59 -0500

    Isomorphic addded

---
+++ Problem_1.py
@@ -0,0 +1,11 @@
+class Solution:
+	def isIsomorphic(self, s: str, t: str) -> bool:
+		if len(set(s)) == len(set(t)):
+			m = {}
+			for i in range(len(s)):
+				if s[i] not in m:
+					m[s[i]] = t[i]
+				elif t[i] != m[s[i]]:
+					return False
+			return True
+		return False
