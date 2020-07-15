de9bfe0d97afd643f3be3291bdeeab30721f34d8
Arpit Sharma <arpit317@gmail.com>
2020-07-14 17:21:49 -0700

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,3 +1,14 @@
+// Time Complexity : O(nk)) 
+// Space Complexity : O(nk)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+// Approach:
+// 1. I used HashMap to store the anagrams togethe against the same key. 
+// 2. To get the same key for anagrams, I used prime numbers multiplication, assigned a different prime number to every character from a to z and then key 
+// for every string was calcualted by multiplying the corresponding prime number for every appearing character.
+// 3. As the multiplication product remains the same irrespective of the order of multiplicants, (a*b*c == a*c*b), all the anagrams were grouped together easily.
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
