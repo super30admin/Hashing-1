405ab69c1db53500cf111cf52017c3b2708a6dd9
jayesh.sathe <jayesh.sathe@gmail.com>
2020-08-24 14:50:36 -0700

    Hashing-1 files implementation

---
+++ IsomorphicString.java
@@ -0,0 +1,27 @@
+package com.Aug2020.Strings;
+
+public class IsomorphicString {
+	public boolean isIsomorphic(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		HashMap<Character, Character> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (map.containsKey(s.charAt(i))) {
+				if (map.get(s.charAt(i)) != t.charAt(i)) {
+					return false;
+				}
+			} else {
+
+				if (map.containsValue(t.charAt(i))) {
+					return false;
+				}
+				map.put(s.charAt(i), t.charAt(i));
+
+			}
+		}
+
+		return true;
+	}
+}
