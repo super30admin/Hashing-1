d8c908ffae3ce75fe368a2238a8ee95242ba136d
akshaykajale <aksh.kajale@gmail.com>
2020-09-18 17:23:53 -0700

    Hashing-1 Completed

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,9 +1,6 @@
 package Trees_Graphs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class IsomorphicStrings {
@@ -16,16 +13,16 @@ public class IsomorphicStrings {
 			return false;
 		}
 		
-		Map<Character, List<Character>> map = new HashMap<>();
+		Map<Character, Character> map = new HashMap<>();
 		
 		for(int i = 0 ; i<s.length() ; i++)
 		{
 			if(!map.containsKey(s.charAt(i)))
 			{
-				map.put(s.charAt(i), new ArrayList<Character>(Arrays.asList(t.charAt(i))));
+				map.put(s.charAt(i), t.charAt(i));
 				
 			}
-			else if(map.get(s.charAt(i)).contains(t.charAt(i)))
+			else if(map.get(s.charAt(i)) == t.charAt(i))
 			{
 				continue;
 			}
