e3d4aeb8be269a0b3b5150cb7ddc936637967a8d
jananduri <jaisurya.nanduri@copart.com>
2020-09-21 19:22:43 -0500

    Finished Hashing-1

--- isomorphicWords.py
+++ isomorphicWords.py
@@ -4,18 +4,17 @@ Space Complexity : O(N)
 Did this code successfully run on Leetcode : yes
 Any problem you faced while coding this : no
 """
-
 import collections
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         
         d_s = collections.defaultdict(list)
         d_t = collections.defaultdict(list)
         
-        for i, char in enumerate(pattern):
+        for i, char in enumerate(s):
             d_s[char].append(i)
         
-        for j, char in enumerate(s.split(" ")):
+        for j, char in enumerate(t):
             d_t[char].append(j)
          # return d_s.values() == d_t.values()
         
