bca50a76c7b2d2eb6ef57c2318abc166cd3b0088
jananduri <jaisurya.nanduri@copart.com>
2020-09-21 19:16:45 -0500

    Finished brute force without comments

---
+++ findMissing.py
@@ -0,0 +1,21 @@
+print("Hello, world!")
+# def binarysearch(nums, target):
+
+#     def condition(value, target):
+#         return nums[value] == target
+    
+#     low = 0
+#     high = len(nums)
+    
+#     while low < high:
+        
+#         mid = low + (high - low)//2
+#         if(condition(mid, target)):
+#             high = mid
+#         else:
+#             low = mid + 1
+            
+#     return low
+
+# a = [0,1,2,3,4,5,6]
+# print(binarysearch(a, 4))
