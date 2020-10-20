800e682d66c4cd4066f53766750ded6cc7652454
GayatriHungund81194 <gayatri811.gh@gmail.com>
2020-10-19 19:47:06 -0700

    completed assignment

--- groupAangrams.py
+++ groupAangrams.py
@@ -1,5 +1,6 @@
-# A very simple way to solve this is using dictionaries. Now, in python there is concept of default dictionary in which I have initialized the value to be in form of list
+# A very simple way to solve this is using dictionaries as discussed in class. Now, in python there is concept of default dictionary in which I have initialized the value to be in form of list
 # But the tricky part here is to understand how to stre the kesy, in anagrams no matter what the order is, all letters are same in all words. so I sorted them and then stored them in dictionary.
+# There was another method discussed using prime product that can also be used to solve this question.
 # Time complexity: O(nlogn)
 # space complexity: O(n)
 import collections
