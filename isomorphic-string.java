c3d3b1de979d0a8ad59f7cd6d182a541bd79d631
Rishi Raj <rishiraj824@gmail.com>
2020-11-24 05:26:06 -0500

    Update isomorphic-string.java

--- isomorphic-string.java
+++ isomorphic-string.java
@@ -8,6 +8,45 @@ class Solution {
             return false;
         }
         
+        char[] a = new char[100];
+        char[] b = new char[100];
+        
+        for(int i=0;i<s.length();i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            
+            if(a[ch1 - ' '] != 0) {
+                if(a[ch1 - ' '] != ch2) {
+                    return false;
+                }
+            }
+            else {
+                a[ch1 - ' '] = ch2;
+            }
+            
+            if(b[ch2 - ' '] != 0) {
+                if(b[ch2- ' '] != ch1) {
+                    return false;
+                }                
+            }
+            else {
+                b[ch2 - ' '] = ch1;
+            }
+        }
+        
+        return true;
+    }
+   
+}
+
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
         HashMap<Character, Character> map1 = new HashMap<Character, Character>();
         HashMap<Character, Character> map2 = new HashMap<Character, Character>();
 
