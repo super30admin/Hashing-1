c9531fdbc81ad311893896c7aa6662f2a4cad701
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-09 23:15:25 -0800

    Update isomorphicStrings205.java

--- isomorphicStrings205.java
+++ isomorphicStrings205.java
@@ -7,35 +7,17 @@ import java.util.HashMap;
 // https://leetcode.com/problems/isomorphic-strings/
 // https://github.com/super30admin/Hashing-1/
 public class isomorphicStrings205 {
-	public boolean isIsomorphic(String s, String t) {
-		if (s.length() != t.length()) {
-			return false;
-		}
-		HashMap<Character, Character> sMap = new HashMap<>();
-		HashMap<Character, Character> tMap = new HashMap<>();
-
-		for (int i = 0; i < s.length(); i++) {
-			char sChar = s.charAt(i);
-			char tChar = t.charAt(i);
-			if (sMap.containsKey(sChar)) {
-				if (sMap.get(sChar) != tChar) {
-					return false;
-				}
-			} else {
-				sMap.put(sChar, tChar);
-			}
-			if (tMap.containsKey(tChar)) {
-				if (tMap.get(tChar) != sChar) {
-					return false;
-				} else {
-					tMap.put(tChar, sChar);
-				}
-			}
-
-		}
-
-		return true;
-	}
+public boolean isIsomorphic (String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
 
 	// Time complexity o(n)
 	// Space complexity o(1)
