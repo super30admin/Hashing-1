e684707b359e81c94efa3dc2860726991de84cdd
Anshul Atriek <atriekak@gmail.com>
2020-12-29 16:16:58 -0500

    Added time and space complexities

--- Word Pattern.py
+++ Word Pattern.py
@@ -1,5 +1,9 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        #Time Complexity: O(n)
+        #Space Complexity: O(1)
+        #where, n equals min(pattern.length, s.length)
+        
         s_list = s.split()
         
         if len(pattern) != len(s_list):
@@ -8,15 +12,17 @@ class Solution:
         p_dict = {}
         s_dict = {}
         
-        for idx in range(len(pattern)):
-            if pattern[idx] in p_dict:
-                p_dict[pattern[idx]].append(idx)
+        for i in range(len(pattern)):
+            if pattern[i] in p_dict:
+                if p_dict[pattern[i]] != s_list[i]:
+                    return False
             else:
-                p_dict[pattern[idx]] = [idx]
+                p_dict[pattern[i]] = s_list[i]
                 
-            if s_list[idx] in s_dict:
-                s_dict[s_list[idx]].append(idx)
+            if s_list[i] in s_dict:
+                if s_dict[s_list[i]] != pattern[i]:
+                    return False
             else:
-                s_dict[s_list[idx]] = [idx]
+                s_dict[s_list[i]] = pattern[i]
         
-        return list(p_dict.values()) == list(s_dict.values())
+        return True
