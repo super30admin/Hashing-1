08478f502a777561cfb3588c3f0d28280e4d1867
Soumyya Kanti Datta <61840898+skrantidatta@users.noreply.github.com>
2020-12-28 23:21:50 -0500

    please review

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,5 +1,5 @@
 #// Time Complexity :O(n*mlogm) #n is no of strings and m is the average size of each word
-#// Space Complexity :O(n)
+#// Space Complexity :O(nk) # k is the no of strings in each group
 #// Did this code successfully run on Leetcode :yes
 #// Any problem you faced while coding this :no
 
