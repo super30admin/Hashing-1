9cdd60082cdac5d4b2217a709c36b8632e021911
Ashish Mishra <ashishk861@gmail.com>
2020-12-30 12:27:17 -0500

    Update Problem_2.java

--- Problem_2.java
+++ Problem_2.java
@@ -1,4 +1,4 @@
-// Time Complexity :O(log(m*n))
+// Time Complexity :O(n)
 // Space Complexity :O(1)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :mapping the the midpoint element to matrix.
@@ -6,4 +6,28 @@
 
 // Your code here along with comments explaining your approach
 
-//one way is the brute force approach which will take O(n^2)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Character> hash=new HashMap<Character,Character>();
+        HashSet<Character> set=new HashSet<Character>();
+        for(int i=0;i<s.length();i++){
+            if(hash.containsKey(s.charAt(i))){
+                if(t.charAt(i)!=hash.get(s.charAt(i))){
+                    return false;
+                }
+            }else{
+                if(set.contains(t.charAt(i))){
+                    return false;
+                }
+                hash.put(s.charAt(i),t.charAt(i));
+                set.add(t.charAt(i));
+            }
+        }
+
+        return true;
+    }
+}
