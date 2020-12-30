70e78015c596da6040289a1d50a1146809044b76
Sai Teja <saitejanagam@gmail.com>
2020-12-30 08:43:00 +0530

    Completed Hashing-1

---
+++ Problem1.java
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/group-anagrams/
+Given an array of strings, group anagrams together.
+Example: Input: ["eat", "tea", "tan", "ate", "nat", "bat"], Output: [ ["ate","eat","tea"], ["nat","tan"], ["bat"] ]
+Note: All inputs will be in lowercase. The order of your output does not matter.
+*/
+
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+
+import java.util.*;
+class Problem1{
+
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char wordArr[] = word.toCharArray();
+            Arrays.sort(wordArr);
+            String sorted_word = new String(wordArr);
+            if (map.containsKey(sorted_word)) {
+                List<String> list = map.get(sorted_word);
+                list.add(word);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                map.put(sorted_word, list);
+            }
+        }
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            res.add(list);
+        }
+        return res;
+    }
+}
---
+++ Problem2.java
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/isomorphic-strings/
+Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if the characters in s can be replaced to get t. All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+Example 1: Input: s = "egg", t = "add" Output: true
+Example 2: Input: s = "foo", t = "bar" Output: false
+Example 3: Input: s = "paper", t = "title" Output: true Note: You may assume both s and t have the same length.
+*/
+
+
+//Time Complexity : O(N)
+//Space Complecity : O(1)
+
+
+import java.util.*;
+class Problem2 {
+
+    
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char map[] = new char[128]; // source -> target
+        char reverseMap[] = new char[128]; // target -> source
+        Arrays.fill(map, '|');
+        Arrays.fill(reverseMap, '|');
+        for (int i = 0; i < s.length(); i++) {
+            char source = s.charAt(i);
+            char target = t.charAt(i);
+            char mapped_character = map[source];
+            char reverse_character = reverseMap[target];
+            if (mapped_character == '|' && reverse_character == '|') { // Not yet mapped
+                map[source] = target;
+                reverseMap[target] = source;
+            } else if (mapped_character == '|' && reverse_character != '|') {
+                if (reverse_character == source) {
+                    map[source] = target;
+                } else {
+                    return false;
+                }
+            } else if (mapped_character != target) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
