d6f0e7d8a84b1dbc121a2436c30e89ccfc69ce29
Ashish Mishra <ashishk861@gmail.com>
2020-12-30 12:15:37 -0500

    Create Problem_2.java

---
+++ Problem_2.java
@@ -0,0 +1,9 @@
+// Time Complexity :O(log(m*n))
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :mapping the the midpoint element to matrix.
+
+
+// Your code here along with comments explaining your approach
+
+//one way is the brute force approach which will take O(n^2)
