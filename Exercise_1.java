c1efb04d04ebe9b3dee47fc9d6b08fde91b2187a
Akhil Varupula <akhilvarupula@Akhils-MacBook.attlocal.net>
2021-03-23 18:07:22 -0700

    completed hashing-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,84 +1,25 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2578
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red87\green96\blue106;\red255\green255\blue255;\red27\green31\blue34;
-\red21\green23\blue26;}
-{\*\expandedcolortbl;;\cssrgb\c41569\c45098\c49020;\cssrgb\c100000\c100000\c100000;\cssrgb\c14118\c16078\c18039;
-\cssrgb\c10588\c12157\c13725\c30196;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
+// Time Complexity : O(nklogk) where n is length of strs and k is the size of biggest String in strs.
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clmgf \clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clmrg \clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
+// Space Complexity :O(n)
 
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-// Time Complexity : O(nklogk) where n is length of strs and k is the size of biggest String in strs.\cf4 \cb1 \cell 
-\pard\intbl\itap1\cell \row
+// Did this code successfully run on Leetcode :yes 
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Space Complexity :O(n)\cf4 \cb1 \cell \row
+// Any problem you faced while coding this :no
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Did this code successfully run on Leetcode :yes \cf4 \cb1 \cell \row
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Any problem you faced while coding this :no\cf4 \cb1 \cell \row
+// Your code here along with comments explaining your approach
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sorted= new String(charArray);
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
+            }map.get(sorted).add(s);
+            }
+        return new ArrayList(map.values());
+        }
+    }
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cb3 \
-\cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cb3 \
-\cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Your code here along with comments explaining your approach\cell \lastrow\row
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f1 \cf0 \cb1 \kerning1\expnd0\expndtw0 class Solution \{\
-    public List<List<String>> groupAnagrams(String[] strs) \{\
-        HashMap<String, List<String>> map = new HashMap<>();\
-        for(String s : strs)\{\
-            char[] charArray = s.toCharArray();\
-            Arrays.sort(charArray);\
-            String sorted= new String(charArray);\
-            if(!map.containsKey(sorted))\{\
-                map.put(sorted,new ArrayList<>());\
-            \}map.get(sorted).add(s);\
-            \}\
-        return new ArrayList(map.values());\
-        \}\
-    \}\
-}
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,94 +1,34 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2578
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red87\green96\blue106;\red255\green255\blue255;\red27\green31\blue34;
-\red21\green23\blue26;}
-{\*\expandedcolortbl;;\cssrgb\c41569\c45098\c49020;\cssrgb\c100000\c100000\c100000;\cssrgb\c14118\c16078\c18039;
-\cssrgb\c10588\c12157\c13725\c30196;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
+// Time Complexity :O(n)
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clmgf \clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clmrg \clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
+// Space Complexity :o(n)
 
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-// Time Complexity :O(n)\cf4 \cb1 \cell 
-\pard\intbl\itap1\cell \row
+// Did this code successfully run on Leetcode :yes
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Space Complexity :o(n)\cf4 \cb1 \cell \row
+// Any problem you faced while coding this : no
 
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Did this code successfully run on Leetcode :yes\cf4 \cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Any problem you faced while coding this : no\cf4 \cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cb3 \
-\cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf4 \cb3 \
-\cb1 \cell \row
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
-\clvertalt \clshdrawnil \clwWidth1000\clftsWidth3 \clminw1000 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clvertalt \clshdrawnil \clwWidth23280\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf5 \cell 
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-\cf2 \cb3 // Your code here along with comments explaining your approach\cell \lastrow\row
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f1 \cf0 \cb1 \kerning1\expnd0\expndtw0 class Solution \{\
-    public boolean wordPattern(String pattern, String s) \{\
-        \
-        char[] patArray= pattern.toCharArray();\
-        String[] strArray = s.split(" ");\
-        if(strArray.length!=patArray.length) return false;\
-        HashMap<Character, String> map1 = new HashMap<>();\
-        HashMap<String,Character> map2 = new HashMap<>();\
-        for(int i=0;i<pattern.length();i++)\{\
-            \
-            if(!map1.containsKey(patArray[i]))\{\
-                map1.put(patArray[i], strArray[i]);\
-            \}else\{\
-                if(!map1.get(patArray[i]).equals(strArray[i])) return false;\
-            \}\
-            if(!map2.containsKey(strArray[i]))\{\
-                map2.put( strArray[i],patArray[i]);\
-            \}else\{\
-                if(!map2.get(strArray[i]).equals(patArray[i])) return false;\
-             \}\
-            \
-     \}\
-      return true;\
-\}\
-\}}
+// Your code here along with comments explaining your approach
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+        char[] patArray= pattern.toCharArray();
+        String[] strArray = s.split(" ");
+        if(strArray.length!=patArray.length) return false;
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String,Character> map2 = new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            
+            if(!map1.containsKey(patArray[i])){
+                map1.put(patArray[i], strArray[i]);
+            }else{
+                if(!map1.get(patArray[i]).equals(strArray[i])) return false;
+            }
+            if(!map2.containsKey(strArray[i])){
+                map2.put( strArray[i],patArray[i]);
+            }else{
+                if(!map2.get(strArray[i]).equals(patArray[i])) return false;
+             }
+            
+     }
+      return true;
+}
+}
