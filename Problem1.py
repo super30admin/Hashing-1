8796554662ce657e0aa93a3e1c9f9e7708f51783
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-05-31 09:59:24 +0530

    Completed Problem1

---
+++ Problem1.py
@@ -0,0 +1,20 @@
+ # Time Complexity : O(n) + O(mlogm) m is length of string
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sortedStr = [sorted(i) for i in strs]
+        sortedStr = ["".join(i) for i in sortedStr]
+        print(sortedStr)
+        a = {}
+        for i in range(len(sortedStr)):
+            if sortedStr[i] not in a:
+                a[sortedStr[i]] = [strs[i]]
+            else:
+                a[sortedStr[i]].append(strs[i])
+        out = []
+        for i in a:
+            out.append(a[i])
+        return out
