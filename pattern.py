6aa27ec23a5ad3f2f630a2f21c7b392b9c1eb7bd
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-06-02 20:28:29 +0530

    Solved Pattern Problem

---
+++ pattern.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        pDic = {}
+        sDic = {}
+        stringList = s.split(" ")
+        
+        if len(pattern) != len(stringList) :
+            return False
+        
+        for i in range(len(pattern)):
+            if pattern[i] not in pDic :
+                pDic[pattern[i]] = stringList[i]
+                
+            else:
+                if pDic.get(pattern[i]) != stringList[i]:
+                    return False
+                
+            if stringList[i] not in sDic:
+                sDic[stringList[i]] = pattern[i]
+                
+            else:
+                if sDic.get(stringList[i]) != pattern[i]:
+                    return False
+                
+                
+        return True
+                 
+        
+      
