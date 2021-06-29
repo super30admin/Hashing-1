2e04ddccfbaa8c9a26608766d9e31d8307176fa4
Sirisha Kona (Student) <skona@asurite.asu.edu>
2021-06-28 21:13:21 -0700

    Updated approach

--- word_pattern.py
+++ word_pattern.py
@@ -6,14 +6,15 @@ class Solution(object):
         :rtype: bool
         """
         p_w={}
+        w_p = {}
         s = s.split()
         if len(pattern) != len(s):
             return False
         for inex,p in enumerate(pattern):
             w = s[inex]
-            print(p,w)
-            if p not in p_w and w not in p_w.values():
+            if p not in p_w and w not in w_p:
                 p_w[p]=w
-            elif p_w.get(p)!=w:
+                w_p[w] = p
+            elif p_w.get(p)!=w and w_p.get!=w:
                 return False
         return True
