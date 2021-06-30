9fca8180dd9c8307d25eeb42c4b0c92836b783ff
sD <shubham.dhingra27@gmail.com>
2021-06-29 19:22:33 -0500

    Update isIsomorphic.py

--- isIsomorphic.py
+++ isIsomorphic.py
@@ -1,28 +1,31 @@
 class Solution:
+    def has_key(self, hashMap, key):
+        if key in hashMap.keys():
+            return True
+        return False
+    
     def isIsomorphic(self, s, t):
+        if not s and not t:
+            return True
         if len(s) != len(t):
             return False
         
-        dic = {}
+        sMap = {}
+        tMap = {}
         
-        for i,j in zip(s,t):
-            dic[i] = j
-            
-        if (len(set(dic.values())) != len(dic.values())):
-            return False
+        for i, j in zip(s,t):
+            if not self.has_key(sMap, i):
+                sMap[i] = j
+            if not self.has_key(tMap, j):
+                tMap[j] = i
+                
+            if (sMap[i] != j or tMap[j] != i):
+                return False
         
-        strng = ''
-        
-        for i in s:
-            strng += dic[i]
-         
-        if (strng == t):
-            return True
-        
-        return False
+        return True
 
 s = Solution()
 s.isIsomorphic('egg', 'add')
 
-# TC: O(n)
-# SC: O(n)
+# TC: O(N)
+# SC: O(1)
