b96d64752655edbaa823f9ad8ba5d9f5102c17ac
Aparna <aparna20dalal@gmail.com>
2021-07-26 18:23:21 -0500

    Problem#14 Word Pattern solution

---
+++ Problem#14_WordPattern.java
@@ -0,0 +1,28 @@
+/*
+ * Time complexity: O(n) to traverse through both arrays
+ * Space complexity: O(n) to store the elements/keys in hashmap
+ * */
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap map_index = new HashMap();
+        String[] words = s.split(" ");
+
+        if (words.length != pattern.length())
+            return false;
+
+        for (Integer i = 0; i < words.length; i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+            if (!map_index.containsKey(c))
+                map_index.put(c, i);
+
+            if (!map_index.containsKey(w))
+                map_index.put(w, i);
+
+            if (map_index.get(c) != map_index.get(w))
+                return false;
+        }
+
+        return true;
+    }
