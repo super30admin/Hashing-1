bb9161c79830546736f19bfbcc1b50e8d16ff207
Aparna <aparna20dalal@gmail.com>
2021-07-26 18:49:43 -0500

    Problem#12 Group Anagrams

---
+++ Problem#12_GroupAnagrams.java
@@ -0,0 +1,18 @@
+/*
+Time Complexity: O(NKlog K) where N is the length of strs, and K is the maximum length of a string in strs. 
+Space Complexity: O(NK)
+*/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
