c2488dac7c4dde7b360d2fdbf54207d84e125901
Adhish Shrivastava <adhish70@gmail.com>
2021-10-12 10:06:53 -0400

    Probelm-1 done

--- Problem-1.py
+++ Problem-1.py
@@ -5,5 +5,26 @@
 # Space Complexity: 
 
 class Solution:
+    def sort(self, s):
+        s = [a for a in s]
+        s.sort()
+        return "".join(s)
+
     def groupAnagrams(self, strs):
-        pass
+        if len(strs) == 1:
+            return [strs]
+
+        temp_dict = dict()
+
+        for i in strs:
+            sorted_i = self.sort(i)
+            if sorted_i in temp_dict:
+                temp_dict[sorted_i].append(i)
+            else:
+                temp_dict[sorted_i] = [i]
+        return list(temp_dict.values())
+
+obj = Solution()
+print(obj.groupAnagrams(['']))
+print(obj.groupAnagrams(['a']))
+print(obj.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
