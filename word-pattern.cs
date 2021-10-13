ac8d1cf4f8fdbb2f845d356bc58ea6c7602534e2
Rishabh <rishabhsarangmehta@gmail.com>
2021-10-13 19:27:11 -0400

    Added solution to Hashing 1, Please review

---
+++ word-pattern.cs
@@ -0,0 +1,43 @@
+﻿// Time complexity is O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode: Yes (https://leetcode.com/submissions/detail/570158347/)
+// Any problem you faced while coding this:
+
+using System;
+using System.Collections;
+
+namespace word_pattern
+{
+    class Program
+    {
+        public static bool WordPattern(string pattern, string s) {
+        Hashtable hashTable = new Hashtable();
+        String[] sArr = s.Split(" ");
+        if(pattern.Length != sArr.Length) {
+            return false;
+        }
+        for(int i =0; i < pattern.Length; i++) {
+            if (!hashTable.ContainsKey(pattern[i])) {
+                if(!hashTable.ContainsValue(sArr[i])) {
+                    hashTable.Add(pattern[i], sArr[i]);
+                }
+                else {
+                    return false;
+                }
+            } else {
+                //check if the pattern at i is equal to corresponsing s
+                if(! hashTable[pattern[i]].ToString().Equals(sArr[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+        static void Main(string[] args)
+        {
+            String pattern = "aaa";
+            String s = "aa aa aa aa";
+            Console.WriteLine(Program.WordPattern(pattern, s));
+        }
+    }
+}
