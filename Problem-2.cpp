cdbba0a368f8c3b6b79fe135e72abed53f8922d7
Durgesh Patil <patildurgesh1998@gmail.com>
2021-10-26 17:51:31 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -1,6 +1,13 @@
 //Time Complexity = O(N)
 //Space Comlexity = O(1)                  //since string only contains the ASCII characters which are 256.
+/*Question: Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if the characters in s can be replaced to get t. All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
 
+Example 1: Input: s = "egg", t = "add" Output: true
+
+Example 2: Input: s = "foo", t = "bar" Output: false
+
+Example 3: Input: s = "paper", t = "title" Output: true Note: You may assume both s and t have the same length.
+*/
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
