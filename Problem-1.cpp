204e393ef378d852b6435ec502449a0754959ad2
Durgesh Patil <patildurgesh1998@gmail.com>
2021-10-26 17:50:59 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -1,6 +1,12 @@
 //Time Complexity = O(NKlogK)          
 //Space Complexity = O(NK)
 //Where N is the length of the vector of string and K is the average length of the invidual string.
+/*Question: Given an array of strings, group anagrams together.
+
+Example: Input: ["eat", "tea", "tan", "ate", "nat", "bat"], Output: [ ["ate","eat","tea"], ["nat","tan"], ["bat"] ]
+
+Note: All inputs will be in lowercase. The order of your output does not matter.
+*/
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
