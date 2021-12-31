8136d82923a85f5ed1db3404b8177726205ec68d
M.Hitesh <54840773+hiteshmadapathi@users.noreply.github.com>
2021-12-31 01:25:27 -0500

    completed group anagrams

---
+++ group_anagrams.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        s = {}
+        for i in range(len(strs)):
+            temp = self.calculateprimeprod(strs[i])
+            if temp not in s.keys():
+                s[temp] = []
+            s[temp].append(strs[i])
+        return s.values()
+    
+    def calculateprimeprod(self,string):
+        primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,67,71,73,79,83,89,97,101,103]
+        product = 1
+        for i in range(len(string)):
+            char = string[i]
+            product = product*primes[ord(char)-ord('a')]
+        return product
