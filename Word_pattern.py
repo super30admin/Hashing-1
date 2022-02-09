7379950d5c2f28b4ef6e25e354966414bb42a050
Mounica5K <98117848+Mounica5K@users.noreply.github.com>
2022-02-08 16:46:27 -0800

    Hashing1 P3

---
+++ Word_pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        hmap_p = {}
+        hmap_s = {}
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+        
+        for i in range(len(pattern)):
+            if pattern[i] not in hmap_p and s[i] not in hmap_s:
+                hmap_p[pattern[i]] = s[i]
+                hmap_s[s[i]] = pattern[i]
+            elif hmap_p.get(pattern[i]) != s[i]:
+                return False
+            
+        return True
