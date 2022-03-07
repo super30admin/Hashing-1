c8c9a394d28207ebf9c127813c6245749f72161d
Yashwant Bezawada <yashwant_b@me.com>
2022-03-07 15:17:15 -0600

    Update SolutionGroupAnagrams.java

--- SolutionGroupAnagrams.java
+++ SolutionGroupAnagrams.java
@@ -1,9 +1,18 @@
+/ Time Complexity : O(nk)
+// Space Complexity : O(nk)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
 class SolutionGroupAnagrams {
     
     int[] primeArray = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
     
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<Double,List<String>> hm = new HashMap<>();
+        
+        //Store all strings in keys as hashcodes (representing each character as a unique prime number) and everytime a string matches that hashcode store it against that hascode)
         for(int i = 0; i < strs.length; i++){
             double pp = primeProduct(strs[i]);
             if(!hm.containsKey(pp)){
@@ -12,7 +21,7 @@ class SolutionGroupAnagrams {
             hm.get(pp).add(strs[i]);
         }
         
-        return new ArrayList<>(hm.values());
+        return new ArrayList<>(hm.values()); //group and return all map values
     }
     
     double primeProduct(String s){
