63aca73e97dc878595b85cb98ccae6b8c458f80c
Aditya Desai <adidesi95@gmail.com>
2022-03-08 04:45:05 -0700

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -21,20 +21,20 @@ import java.util.List;
 import java.util.Map;
 
 public class Anagram {
-    static final int[] primeNums = {2,3,5,7,11,13,17,19,23,29,31,37,41,47,53,59,61,67,71,73,79,83,89,97};
-    private static int calculateHash(String str){
-        int hash = 0;
+    static final int[] primeNums = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+    private static double calculateHash(String str){
+        double hash = 1;
         for (int i = 0; i < str.length(); i++) {
-            hash += primeNums[str.charAt(i) - 'a'] * (str.charAt(i));
+            hash *= primeNums[str.charAt(i) - 'a'];
         }
         return hash;
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<Integer, List<String>> map = new HashMap<>();
+        Map<Double, List<String>> map = new HashMap<>();
         for(String str : strs){
-            int hash = calculateHash(str);
-            System.out.println(str+" : "+hash);
+            double hash = calculateHash(str);
+            
             map.computeIfAbsent(hash, k->new ArrayList<>());
             map.get(hash).add(str);
         }
