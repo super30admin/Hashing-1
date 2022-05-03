591dd975c356d128a96713d32d6960329bfc8384
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-03 19:29:26 -0400

    Update groupAnagrams.java

--- groupAnagrams.java
+++ groupAnagrams.java
@@ -1,12 +1,14 @@
 // O(w*n) | O(wn) where w is the number of words and n is the length of the longest word
-
+//First Perform a linear search over the given since we need to cover all the elements , so the search space
+// includes the whole array, And then we need to the maintain a hashmap where we need to keep a track of
+// the elements and find its pairing while performing the iteration
 
 class Solution{
-public static List<List<String>> groupAnagrams(List<String> words) {
+public static List<List<String>> groupAnagrams(String[] strs) {
     
 		Map<String , List<String>> anagrams = new HashMap<String, List<String>>();
 		
-		for (String word : words){
+		for (String word : strs){
 	
 				char[]  charArray = word.toCharArray();
 				Arrays.sort(charArray);
