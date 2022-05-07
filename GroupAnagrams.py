ca25f35f470825f7b80c1497690f4833fe43838b
Charishma090619999 <93498653+Charishma090619999@users.noreply.github.com>
2022-05-06 19:57:32 -0700

    commited hashing1

---
+++ GroupAnagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        k=0
+        lst=strs.copy()
+        for i in range(len(lst)):
+            lst[i]="".join(sorted(lst[i]))
+        s=set(lst)   
+        result=[[]]*len(s)
+        print(result)
+        for i in s:
+            result[k]=[]
+            for j in range(len(lst)):
+                if(lst[j]==i):
+                    result[k].append(strs[j])
+            k+=1
+        return result
