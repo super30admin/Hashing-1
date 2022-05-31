f912d9110f4c4fc07b4fceab97da429159195510
Sandeep Kumar Reddy Onteddy (Student) <sonteddy@sundevils.asu.edu>
2022-05-31 01:35:54 -0700

    Completed Hashing-1 Please review

--- isomorphic-strings.py
+++ isomorphic-strings.py
@@ -17,3 +17,24 @@ class Solution:
             else:
                 tmap[t_char]=s_char
         return True
+        
+# Using map and set
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t): return False
+        mp=dict()
+        st=set()
+        for i in range(len(s)):
+            s_char=s[i]
+            t_char=t[i]
+            if s_char not in mp:
+                if t_char in st:
+                    return False
+                mp[s_char]=t_char
+                st.add(t_char)
+            else:
+                if mp[s_char]!=t_char: return False
+            
+        return True
