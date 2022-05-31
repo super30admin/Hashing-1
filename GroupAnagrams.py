e0e4f76a1027055d13cbae177406cb9598d9f004
plokaiah <plokaiah@asu.edu>
2022-05-31 01:25:07 -0700

    Completed hashing 1

---
+++ GroupAnagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict={}
+        
+        for i in range(0,len(strs)):
+            sortkey="".join(sorted(strs[i]))
+            if sortkey not in dict:
+                dict[sortkey]=[strs[i]]
+            else:
+                dict[sortkey].append(strs[i])
+        return dict.values()
