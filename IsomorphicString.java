075076e1f422da1144e865943e1abf925696df08
Damandeep Singh <sdamandeep18@gmail.com>
2022-05-30 20:04:15 -0700

    Completed Sprint Hashing-1

---
+++ IsomorphicString.java
@@ -0,0 +1,33 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()){
+        return false;
+    }
+    //implement hashmap
+    HashMap<Character, Character> map1 = new HashMap<>();
+    HashMap<Character, Character> map2 = new HashMap<>();
+ 
+    for(int i=0; i<s.length(); i++){
+        char c1 = s.charAt(i);
+        char c2 = t.charAt(i);
+ 
+        if(map1.containsKey(c1)){
+            if(c2!=map1.get(c1)){
+                return false;
+            }
+        }else{
+            if(map2.containsKey(c2)){
+                return false;
+            }
+ 
+            map1.put(c1, c2);
+            map2.put(c2, c1);
+        }
+    }
+ 
+    return true;
+ }
+}
---
+++ WordPattern.java
@@ -0,0 +1,20 @@
+// Time complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.*;
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+    String[] words = s.split(" ");
+    if (words.length != pattern.length())
+      return false;
+
+    Map<Character, Integer> charToIndex = new HashMap<>();
+    Map<String, Integer> stringToIndex = new HashMap<>();
+
+    for (Integer i = 0; i < pattern.length(); ++i)
+      if (charToIndex.put(pattern.charAt(i), i) != stringToIndex.put(words[i], i))
+        return false;
+
+    return true;
+    }
+}
