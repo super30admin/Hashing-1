fbe7971d52024616a66f3ab56e3c4561fddc21de
Krina Patel <kp42942n@pace.edu>
2022-06-07 19:47:17 -0400

    Update isIsomorphic.js

--- isIsomorphic.js
+++ isIsomorphic.js
@@ -8,26 +8,43 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function (s, t) {
-  const map = {};
-  const mapT = {};
-  for (let i = 0; i < s.length; i++) {
-    const charS = s[i];
-    const charT = t[i];
-    if (map[charS]) {
-      if (map[charS] !== charT) {
-        return false;
+
+ var isIsomorphic = function(s, t) {
+  let sMap = new Map();
+  let tMap = new Map();
+  for(let i = 0; i < s.length; i++) {
+      if(sMap.has(s[i]) && sMap.get(s[i]) !== t[i]) {
+          return false;
       }
-    } else {
-      map[charS] = charT;
-    }
-    if (mapT[charT]) {
-      if (mapT[charT] !== charS) {
-        return false;
+      if(tMap.has(t[i]) && tMap.get(t[i]) !== s[i]) {
+          return false;
       }
-    } else {
-      mapT[charT] = charS;
-    }
+      sMap.set(s[i],t[i]);
+      tMap.set(t[i],s[i]);
   }
   return true;
 };
+
+// var isIsomorphic = function (s, t) {
+//   const map = {};
+//   const mapT = {};
+//   for (let i = 0; i < s.length; i++) {
+//     const charS = s[i];
+//     const charT = t[i];
+//     if (map[charS]) {
+//       if (map[charS] !== charT) {
+//         return false;
+//       }
+//     } else {
+//       map[charS] = charT;
+//     }
+//     if (mapT[charT]) {
+//       if (mapT[charT] !== charS) {
+//         return false;
+//       }
+//     } else {
+//       mapT[charT] = charS;
+//     }
+//   }
+//   return true;
+// };
