becb8bb3e7ec38617ba33ef3b7f8bcd288196151
spatil29bing <86124914+spatil29bing@users.noreply.github.com>
2022-07-07 01:00:19 -0400

    Create wordPattern_setNmap.java

---
+++ wordPattern_setNmap.java
@@ -0,0 +1,33 @@
+//Time Complexity: O(n)
+//Space Complexity : O(n)
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> charMap = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        
+        String[] sArray = s.split(" ");
+        
+        if(pattern.length() != sArray.length)return false;
+        
+        for(int i = 0; i < sArray.length; i++)
+        {
+            char ch = pattern.charAt(i);
+            
+            if(charMap.containsKey(ch))
+            {
+                String mapped = charMap.get(ch);
+                if(!mapped.equals(sArray[i])) return false;
+            }
+            else
+            {
+                if(set.contains(sArray[i]))return false;
+            }
+            charMap.put(ch, sArray[i]);
+            set.add(sArray[i]);
+        }
+        
+        
+        return true;
+        
+    }
+}
