5305cdb32d5d2aced1e981640088c29182ae23d6
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-25 20:00:00 -0400

    Create wordpattern.java

---
+++ wordpattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character,String> pMap = new HashMap<>();
+        String[] sArray = s.split(" ");
+        if(pattern.length()!= sArray.length)
+            return false;
+        Set<String> set = new HashSet<>();
+        for(int i =0;i<pattern.length();i++){
+            char pChar = pattern.charAt(i);
+            String str = sArray[i];
+            if(pMap.containsKey(pChar)){
+                if(!(pMap.get(pChar)).equals(str))
+                    return false;
+            }
+            else{
+                if(set.contains(str))
+                    return false;
+                pMap.put(pChar,str);
+                set.add(str);
+            }
+        }
+        return true;
+    }
+}
