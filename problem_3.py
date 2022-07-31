ccbe6b73267ae55089d0f972381d9e47ce861264
Chandra Kiran Viswanath Balusu <cbalusu@gmu.edu>
2022-07-31 10:50:24 -0500

    Commit Staged Changes

---
+++ problem_3.py
@@ -0,0 +1,22 @@
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_map, t_map = dict(), dict()
+        t = t.split(" ")
+        for i in range(len(s)):
+            if s[i] in s_map:
+                if s_map[s[i]] != t[i]:
+                    return False
+            else:
+                s_map[s[i]] = t[i]
+            if t[i] in t_map:
+                if t_map[t[i]] != s[i]:
+                    return False
+            else:
+                t_map[t[i]] = s[i]
+        return True
+
+
+if __name__ == '__main__':
+    print(Solution().isIsomorphic("abba", "dog cat cat dog"))
