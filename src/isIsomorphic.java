f8fb2f3d0f9fd5e78a4362c93cdccbf437a3fe58
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-08-22 21:39:24 -0400

    Ismorphic String

---
+++ src/isIsomorphic.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+// Time Complexity : o(n)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+public class isIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Character> smap = new HashMap<>();
+        HashMap<Character, Character> tmap = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+
+            char schar = s.charAt(i);
+            char tchar = t.charAt(i);
+
+            if( smap.containsKey(schar)){
+                if(smap.get(schar) != tchar){
+                    return false;
+                }
+            }else{
+                smap.put(schar,tchar);
+            }
+
+            if( tmap.containsKey(tchar)){
+                if(tmap.get(tchar) != schar){
+                    return false;
+                }
+            }else{
+                tmap.put(tchar,schar);
+            }
+
+        }
+
+        return true;
+
+    }
+
+}
