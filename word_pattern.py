975987492156ca53f9d361b3ad65ef3cc443b07b
Siddhant Shah <siddhanta.shah@st.niituniversity.in>
2022-08-29 13:29:05 -0700

    Completed word_patern

--- word_pattern.py
+++ word_pattern.py
@@ -1,14 +1,28 @@
-def word_pattern(pattern, str):
-    str_list = str.split(" ")
-    if len(pattern) != len(str_list):
-        return False
-
-    hm1, hm2 = {}, {}
-    for i in range(len(pattern)):
-        c1, c2 = pattern[i], str_list[i]
-        if c1 in hm1 and hm1[c1] != c2 or c2 in hm2 and hm2[c2] != c1:
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        string = s.split(' ')
+        
+        if len(pattern) != len(string):
+            print("Inside")
             return False
-        hm1[c1] = c2
-        hm2[c2] = c1
-
-    return True
+        
+        pmap={}
+        smap={}
+        
+        for i in range(len(pattern)):
+            if pattern[i] in pmap:
+                if pmap[pattern[i]]!=string[i]:
+                    return False
+            else:
+                pmap[pattern[i]]=string[i]
+            
+            if string[i] in smap:
+                if smap[string[i]]!=pattern[i]:
+                    return False
+            else:
+                smap[string[i]]=pattern[i]
+        
+        print(smap)
+        print(pmap)
+        
+        return True
