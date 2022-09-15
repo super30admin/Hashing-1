4b711d6b1086443b31b47a70c3a3a387131389da
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-09-15 17:43:13 -0500

    Leetcode 49, Group anagram, Sorting/B-F approach added.

--- LeetCode49_GroupAnagrams.java
+++ LeetCode49_GroupAnagrams.java
@@ -38,3 +38,44 @@ class Solution {
         return primeProduct;
     }
 }
+
+
+
+//approach -2
+//take individual and store it in a string. get a char array for it. 
+//Sort char array
+//convert that sorted array to string. 
+// now, sorted string --put as key to hashmap, Arraylist as value. 
+//add original string to value. 
+//everytimg keep checking for sorted string, if exists add to the val else add new key-val pair 
+//sorting - O(k log k), for n strings - O(nk log k);, considering Hashmap lookup constant. 
+//Time - O(nk logk)
+//space - O(n)
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0) return new ArrayList<>();
+        
+        HashMap<Double, List<String>> map = new HashMap<>();
+        
+        for(int i  = 0; i< strs.length; i++)
+        {
+			String str = strs[i];
+			char[] strArray = str.toCharArray();
+			strArray.sort();
+			String sorted_str = String.valueOf(strArray);
+                       
+            if(!map.containsKey(sorted_str))
+            {
+                map.put(sorted_str, new ArrayList<>());
+               
+            }
+            map.get(sorted_str).add(str);
+        }
+        return new ArrayList<>(map.values());
+       
+    }
+       
+}
