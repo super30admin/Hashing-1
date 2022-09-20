9be5f7765eed62725bbbb6aa58f160f3b56215a2
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-19 19:42:09 -0700

    Update groupAnagrams.java

--- groupAnagrams.java
+++ groupAnagrams.java
@@ -1,5 +1,7 @@
 // TC: O(nk)
 // SC: O(n)
+// Solved on Leetcode
+// No problems faced
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
