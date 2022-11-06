8772e235ea24327611f3e1fb44d02188709ae1f3
ZeelSatasiya <zeel.satasiya@gmail.com>
2022-11-05 22:52:26 -0700

    Hashing-1 Completed

--- Problem_2.java
+++ Problem_2.java
@@ -1,30 +1,34 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity :O(n)
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
 
-// 409. Longest Palindrome
+// 205. Isomorphic Strings
 class Solution {
-    public int longestPalindrome(String s) {
-        if(s.length() == 0) return 0;
-        HashSet<Character> map = new HashSet<>();
-        int length = 0;
-        for(int i = 0; i<s.length(); i++){
-           char a = s.charAt(i); 
-            if(map.contains(a)){
-                length += 2;
-                map.remove(a);
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() == 0 || t.length() == 0) return false;
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Character> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a)){
+                if(map.get(a) != b){
+                    return false;
+                }
             }
             else{
-                map.add(a);
+                if(set.contains(b)){
+                    return false;
+                }
+                map.put(a,b);
+                set.add(b);
             }
         }
-        if(!map.isEmpty()){
-            length += 1;
-        }
-        return length;
+        return true;
     }
 }
