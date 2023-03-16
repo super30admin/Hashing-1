12762b7785b8f9574c1a2067e21aae918503f05d
Vamshi Krishna Navulla <vnavulla@gmail.com>
2023-03-16 16:44:16 -0400

    Completed Problem 2 of Hashing 1

---
+++ Problem2.py
@@ -0,0 +1,19 @@
+s = "paper"
+t = "tiblk"
+dict1 = {}
+for char in s:
+    if(char in dict1.keys()):
+        dict1[char] +=1
+    else:
+        dict1[char] = 1
+dict2 = {}
+for char in t:
+    if(char in dict2.keys()):
+        dict2[char] +=1
+    else:
+        dict2[char] = 1
+
+if(list(dict1.values()) == list(dict2.values())):
+    print('True')
+else:
+    print('False')
