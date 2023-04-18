5826ce1064d601af386cb0fc545ee18aa13992f5
Apurva Kemkar <apurvakemkar@Apurvas-MBP.lan>
2023-04-17 21:44:22 -0500

    Hashing-1 part 1, isomorphic strings completed. Please review, Thank you.

--- IsomorphicMap.java
+++ IsomorphicMap.java
@@ -1,7 +1,8 @@
 // Time Complexity : 
 // Space Complexity :   
 // Did this code successfully run on Leetcode : No 
-// Any problem you faced while coding this : This approach was discusses in the class, but not coded, I tried to implement but got stuck
+// Any problem you faced while coding this : YES
+//This approach was discusses in the class, but not coded, I tried to implement but got stuck
 // thinking all these funtion call to repalcr, remove takes time internally and fails for multiple test case due to 
 //s_ = s_.replace(String.valueOf(sChar), "");
 //t_ = t_.replace(String.valueOf(tChar), "");
