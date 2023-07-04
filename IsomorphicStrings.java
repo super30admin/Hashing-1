4a6cfa8b79f5f73d3dafe5b38a63f02f4676525e
somonnoy <sb7238@g.rit.edu>
2023-07-03 23:05:42 -0400

    Completed Isomorphic Strings

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -4,28 +4,43 @@ import java.util.*;
 class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
 
+        // Create a HashMap to store the mapping between ascii value of characters of s and t
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Iterate through the characters of s and t
         for(int i = 0; i < s.length(); i++)
         {
+            // Get the ASCII values of the characters at position i in s and t
             int ascii1 = (int) s.charAt(i);
             int ascii2 = (int) t.charAt(i);
 
-            if(!map.containsKey(ascii1))
+            // If the ascii value of character in s is already mapped to a different ascii value of character in t, then the mapping is not isomorphic, so return false
+            if(map.containsKey(ascii1))
             {
-                map.put(ascii1, ascii2);
+
+                if(map.get(ascii1) != ascii2)
+                {
+                    return false;
+                }
+
             }
 
+
+            // If the ascii value of character in s is not mapped yet, but the ascii value of character in t is already mapped to another ascii value of character in s, then the mapping is not isomorphic, so return false
             else
             {
-                if(map.get(ascii1) != ascii2)
+                if(map.containsValue(ascii2))
                 {
                     return false;
                 }
             }
 
+            // Add the mapping between the ascii value of characters of s and t to the HashMap
+            map.put(ascii1, ascii2);
+
         }
 
+        // All ascii values of characters in s and t have been mapped uniquely, so return true
         return true;
 
     }
