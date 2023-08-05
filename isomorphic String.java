6dc6651e0f3e45324951a8c812d9bfce34e10c79
sangram vuppala <vuppala.s@northeastern.edu>
2023-08-05 20:27:04 +0530

    Isomorphic Strings Solved

---
+++ isomorphic String.java
@@ -0,0 +1,21 @@
+//Time Complexity = O(N)
+//Space Complexity = O(2N)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            map1.put(s.charAt(i), t.charAt(i));
+            map2.put(t.charAt(i), s.charAt(i));
+        }
+        for(int i = 0; i < s.length(); i++) {
+            if(map1.get(s.charAt(i)) != t.charAt(i) || map2.get(t.charAt(i)) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+}
