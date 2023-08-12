9112ebee253f07130825db92530ab71ecc49fca8
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-12 08:50:12 -0700

    Update and rename Problem2_Array to Problem2_Array.java

--- Problem2_Array.java
+++ Problem2_Array.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s==null && t==null){
