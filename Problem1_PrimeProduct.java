5b4dce4838f249ac29fe2c19244bcaf9517ad288
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-12 10:14:01 -0700

    Update Problem1_PrimeProduct.java

--- Problem1_PrimeProduct.java
+++ Problem1_PrimeProduct.java
@@ -1,3 +1,10 @@
+// N = Total number of words
+// K = Average length of each word
+// Time Complexity : O(NK)
+// Space Complexity : O(Nk)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 class Solution {
     public static double getPrimeProductKey(String s) {
         int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101,103};
