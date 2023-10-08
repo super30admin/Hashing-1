1f1c2bf18d1b16b39cea0ba1930414ff773a4d57
EC2 Default User <ec2-user@ip-172-31-37-149.ec2.internal>
2023-10-08 07:10:26 +0000

    update prob 1

--- Prob_1_group_anagram.py
+++ Prob_1_group_anagram.py
@@ -4,9 +4,9 @@ class Solution:
         Time Complexity: O(nklog(k) + k)
         Space Complexity: O(nk)
 
-        Did this code successfully run on Leetcode:
+        Did this code successfully run on Leetcode: Yes
 
-        Any problem you faced while coding this:
+        Any problem you faced while coding this: No
 
         Your code here along with comments explaining your approach:
         The approach: 
