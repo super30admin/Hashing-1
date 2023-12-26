9af5d9112502e3a0111cc6683e9e2d797e9156dc
God7wyn <jamesgodwynj@gmail.com>
2023-12-25 20:18:18 -0500

    Updated Problem 1 with Time complexity O(n*k)

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,5 +1,5 @@
 /*
- * Time Complexity : O(n)
+ * Time Complexity : O(n*k)
  * Space Complexity : O(n)
  * Did this code successfully run on Leetcode : Yes
  * Any problem you faced while coding this : No
@@ -7,10 +7,11 @@
 
  ? Your code here along with comments explaining your approach
    I created a hashmap to keep track of the anagrams that I am
-   grouping together. I sort the string values first and then
-   use then as keys. This allows me to get find the words which
-   are anagrams and then I add it to a list and run a for loop
-   at the end to get all the grouped anagrams as result.
+   grouping together. I do counting sort of the string values
+   first and then use then as keys. This allows me to get find
+   the words which are anagrams and then I add it to a list and
+   run a for loop at the end to get all the grouped anagrams as
+   result.
 */
 
 #include <vector>
@@ -18,14 +19,27 @@
 #include <unordered_map>
 
 class Solution {
+private:
+    std::string countingSort(const std::string& s) {
+        int counter[26] = {0};
+        for (const char& c : s) {
+            counter[c - 'a']++;
+        }
+
+        std::string key;
+        for (int i = 0; i < 26; i++) {
+            key += std::string(counter[i], i + 'a');
+        }
+
+        return key;
+    }
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
         std::unordered_map<std::string, std::vector<std::string>> map;
 
-        for (const std::string& s : strs) {
-            std::string key = s;
-            sort(key.begin(), key.end());
-            map[key].push_back(s);
+
+        for (const std::string& str : strs) {
+            map[countingSort(str)].push_back(str);
         }
 
         std::vector<std::vector<std::string>> result;
