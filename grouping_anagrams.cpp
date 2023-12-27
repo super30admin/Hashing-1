2e39ef0913496bdb6164230b6dfea5de5caa407d
Srihitha J <srihithaj@Srihithas-MacBook-Air.local>
2023-12-27 15:39:18 -0800

    pushed updated code

--- grouping_anagrams.cpp
+++ grouping_anagrams.cpp
@@ -12,30 +12,32 @@
 #include<string>
 #include<unordered_map>
 
+// Method - 2
+
 class Solution {
 public:
-    double primeProduct(std::string s)
+    long double primeProduct(std::string s)
     {
-        double res = 1;
+        long double res = 1;
         for(char c:s)
         {
             res = res * primes[c - 'a'];
         }
         return res;
     }
-    int primes[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 91, 101, 103};
+    int primes[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
         if (strs.empty())
         {
             return {{}};
         }
 
-        std::unordered_map<double, std::vector<std::string>> storage;
+        std::unordered_map<long double, std::vector<std::string>> storage;
         std::vector<std::vector<std::string>> result;
 
         for(int i = 0; i< strs.size(); i++)
         {
-            double pProduct = primeProduct(strs[i]);
+            long double pProduct = primeProduct(strs[i]);
             storage[pProduct].push_back(strs[i]);
         }
         for(auto elem : storage)
