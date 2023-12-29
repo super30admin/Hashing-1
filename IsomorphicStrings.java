b7ae5b7a89c0840bc52de943c3df79cd727764d1
apurvazawar <zawar.ap@northeastern.edu>
2023-12-28 21:10:43 -0600

    Completed  Hashing-1

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class IsomorphicStrings {
+
+
+class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> smap = new HashMap<>();
+        HashSet<Character> tset = new HashSet<>();
+
+        char[] schar = s.toCharArray();
+        char[] tchar = t.toCharArray();
+
+        if(schar.length != tchar.length) return false;
+
+        for(int i = 0; i < tchar.length; i++) {
+            if(smap.containsKey(schar[i])) {
+                if(smap.get(schar[i]) != tchar[i]) return false;
+            } else {
+                if(!tset.contains(tchar[i])){
+                    tset.add(tchar[i]);
+                    smap.put(schar[i], tchar[i]);
+                } else return false;
+            }
+        }
+
+        return true;
+    }
 }
