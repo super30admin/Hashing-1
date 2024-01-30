1400dd6913409fe2adcfa32d6785d3ab929593fb
Amanjot Singh <amanjsingh@Amanjots-MacBook-Air.local>
2024-01-30 12:42:52 -0400

    Word Pattern

---
+++ WordPattern.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//Time Complexity - O(n) => s.length()
+// Space Complexity - O(n) => n=length of pattern (HashMap keys)
+
+public class WordPattern {
+    class Solution {
+        public boolean wordPattern(String pattern, String s) {
+            Map<Character, String> patternToStringMap = new HashMap<>();
+            Set<String> stringSet = new HashSet<>();
+
+            String[] stringArray = s.split(" ");
+
+            if (pattern.length() != stringArray.length) {
+                return false;
+            }
+
+            for (int i = 0; i < pattern.length(); i++) {
+                char currChar = pattern.charAt(i);
+                String currentString = stringArray[i];
+                if (patternToStringMap.containsKey(currChar)) {
+                    if (!patternToStringMap.get(currChar).equals(currentString)) {
+                        return false;
+                    }
+                } else if (stringSet.contains(currentString)) {
+                    return false;
+                } else {
+                    patternToStringMap.put(currChar, currentString);
+                    stringSet.add(currentString);
+                }
+            }
+            return true;
+        }
+    }
+}
