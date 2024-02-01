8f6c81265fae18952e1cf5c58a2eb95a9ad966a0
Sriram Akella <Sriram.Akella@gmail.com>
2024-02-01 15:34:48 -0700

    250 - isomorphic strings

--- 205_isIsomorphic.py
+++ 205_isIsomorphic.py
@@ -20,3 +20,8 @@ class Solution:
             else:
                 tMap[j] = i
         return True
+
+
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         return [*map(s.index, s)] == [*map(t.index, t)]
