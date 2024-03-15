608b75422db20e101f0f4852700a4f7929759c44
AryanReddyy <aryan02reddy@gmail.com>
2024-03-15 14:59:57 -0400

    Hashing-1

--- WordPattern.java
+++ WordPattern.java
@@ -1,30 +1,28 @@
-// TC:
-// SC:
+// TC: O(nk)
+// SC: O(nk)
 
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class WordPattern {
     public boolean wordPattern(String pattern, String s) {
+
+        String[] str = s.split(" ");
+
         HashMap<Character, String> map = new HashMap<>();
         HashSet<String> set = new HashSet<>();
 
-        String[] sa = s.split(" ");
-
-        if (sa.length != pattern.length())
-            return false;
+        if (str.length != pattern.length()) return false;
 
         for (int i = 0; i < pattern.length(); i++) {
-            if (map.containsKey(pattern.charAt(i))) {
-                if (!map.get(pattern.charAt(i)).equals(sa[i])) {
+            if (!map.containsKey(pattern.charAt(i))) {
+                map.put(pattern.charAt(i), str[i]);
+                if (set.contains(str[i])) return false;
+                else set.add(str[i]);
+            } else {
+                if (!map.get(pattern.charAt(i)).equals(str[i])) {
                     return false;
                 }
-            } else {
-                map.put(pattern.charAt(i), sa[i]);
-                if (set.contains(sa[i]))
-                    return false;
-                else
-                    set.add(sa[i]);
             }
         }
         return true;
