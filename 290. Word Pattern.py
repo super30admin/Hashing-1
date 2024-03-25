221295c8cb515cf4a5da0f905a19ae95818869f0
irasharma13 <irasharma13@gmail.com>
2024-03-25 11:41:34 -0700

    added word pattern

---
+++ 290. Word Pattern.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        pattern_map = {}
+        s_map = {}
+        s_split = s.split(" ")
+        if len(s_split) != len(pattern):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in pattern_map:
+                pattern_map[pattern[i]] = s_split[i]
+            elif pattern_map[pattern[i]] != s_split[i]:
+                return False
+            if s_split[i] not in s_map:
+                s_map[s_split[i]] = pattern[i]
+            elif s_map[s_split[i]] != pattern[i]:
+                return False
+        return True
+        
